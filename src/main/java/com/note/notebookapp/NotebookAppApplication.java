4d89614dc1e3a6b8ec1b52bcc463b43ec41709fe
Kateryna Oblakevych <koblakevych@brightgrove.com>
2023-04-26 17:53:40 +0300

    Initial commit.

---
+++ src/main/java/com/note/notebookapp/NotebookAppApplication.java
@@ -0,0 +1,13 @@
+package com.note.notebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/note/notebookapp/NotebookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.notebookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
